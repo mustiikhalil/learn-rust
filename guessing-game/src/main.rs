d2528fd7433829905f40de0e747b208c4df4d002
mustii <mustii@mmk.one>
2019-12-28 22:42:50 +0300

    Finished the tutorial building the guessing game

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,3 +1,26 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("guess a number");
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("some error happened");
+        
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Less than secret number"),
+            Ordering::Greater => println!("Greater than secret number"),
+            Ordering::Equal => {
+                println!("You won!");
+                break;
+            }
+        }
+    }
 }
