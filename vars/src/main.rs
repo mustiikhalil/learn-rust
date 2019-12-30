b52ceb81565d6ed11c70d31de69c99aceeeafc65
mustii <mustii@mmk.one>
2019-12-30 21:27:23 +0300

    Chapter 3, conditions

--- vars/src/main.rs
+++ vars/src/main.rs
@@ -14,6 +14,39 @@ fn main() {
     };
 
     println!("second: {}", second_value);
+
+    let val = if meaning_of_life == 42 {
+        "Bunnies are the best!"
+    } else {
+        "Bunnies are still the best!"
+    };
+    if meaning_of_life < 10 {
+        println!("less than")
+    } else if meaning_of_life > 70 {
+        println!("more than")
+    } else {
+        println!("else")
+    }
+    println!("val: {}", val);
+
+    let mut pos = 0;
+    let result = loop {
+        if pos / 2 == 5 {
+            break pos + 7;
+        }
+        pos += 1;
+    };
+    println!("result: {}", result);
+
+    let a = [10, 20, 30];
+    for elem in a.iter() {
+        println!("element: {}", elem);
+    }
+
+    for i in (1..4).rev() {
+        println!("{}!", i)
+    }
+    println!("LIFTOFF!!!");
 }
 
 fn mutateptr(x: *mut i32) {
