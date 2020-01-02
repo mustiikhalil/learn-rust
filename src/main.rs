aac6d8eb89c7712cac32b7ddff23b006235ffa25
mustii <mustii@mmk.one>
2020-01-02 23:42:02 +0300

    Started chapter 4

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// data with fixed size will be allocated into the stack
+// data that might have different sizes / or doesnt have a known size at compile time in the future will be stored in the heap
+
+// ownership rules
+// 1- each value in rust has it's own owner
+// 2- there can be only one owner at a time
+// 3- when the owner goes out of scope the value will be dropped
 fn main() {
     println!("Hello, world!");
 }
