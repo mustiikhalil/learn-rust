f1cfe3b2d3935b2c7b8bafdfa95d95dfba437772
mustii <mustii@mmk.one>
2020-01-05 08:25:17 +0300

    started with ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -7,4 +7,12 @@
 // 3- when the owner goes out of scope the value will be dropped
 fn main() {
     println!("Hello, world!");
+
+    let s = String::from("Hello");
+    let s2 = s; // this called a move and it invalidates s
+    let s1 = s2.clone(); // clones the data on the heap and creates a new pointer to it
+    println!("{}", s2);
+    println!("{}", s1);
 }
+// at the end of the scope all the values will get dropped
+// even s, it will be dropped when rust calls `drop` which is similar to Resource Acquisition Is Initialization (RAII)
